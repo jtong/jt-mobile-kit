4e0eb5e4b11662775c86b9971b3456e794ca0b26
jtong <tj19832@gmail.com>
2013-07-25 16:39:28 +0800

    jtong - can generate view, routing and scaffold, and fix jtc missing #! issue, step is too large,sorry

--- lib/jt-mobile-kit/gen_routing/gen_routing.rb
+++ lib/jt-mobile-kit/gen_routing/gen_routing.rb
@@ -1,6 +1,6 @@
 require 'erb'
 
-class GenController < Thor::Group
+class GenRouting < Thor::Group
   include Thor::Actions
   source_root File.expand_path("../templates", __FILE__)
   def initialize(args=[], options={}, config={})
@@ -11,8 +11,10 @@ class GenController < Thor::Group
   argument :name
 
   def gen
+    page_name = name
     controller_name = name
-    content = ERB.new File.read("#{GenController.source_root}/controller.js.erb")
-    create_file "js/controller/#{controller_name}_controller.js", content.result(binding)
+    content = ERB.new File.read("#{GenRouting.source_root}/routing.js.erb")
+    inject_into_file "js/routes.js",content.result(binding), :after => "//routing generate"
   end
+
 end
--- lib/jt-mobile-kit/jt_generator.rb
+++ lib/jt-mobile-kit/jt_generator.rb
@@ -1,12 +1,26 @@
 require 'thor'
 require 'jt-mobile-kit/gen_controller/gen_controller'
+require 'jt-mobile-kit/gen_view/gen_view'
+require 'jt-mobile-kit/gen_routing/gen_routing'
+
 
 class JtGenerator < Thor
   include Thor::Actions
   source_root File.expand_path("../", __FILE__)
 
   register GenController, :controller, "controller", "Generate Controller"
+  register GenView, :view, "view", "Generate view"
+  register GenRouting, :routing, "routing", "Generate routing"
+
+  desc :scaffold, "generate scaffold"
+  def scaffold name
+    GenController.start [name]
+    GenView.start [name]
+    GenRouting.start [name]
+  end
 end
+
+
 
 class JtMain < Thor
   register JtGenerator, :g, "g", "Generate something"
